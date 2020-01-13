68cf3e9d42dc0447946fc7bdae6fb1509e8804c1
Akamolyhen <907446875@qq.com>
2020-01-13 15:25:53 +0800

    1——13

--- 川渝/test.c
+++ 川渝/test.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    printf("hehe\n");
-    return 0;
+    int k=3;
+    int nums[7]={1,2,3,4,5,6,7};
+    int temp;
+        int n=sizeof(nums)/sizeof(int);
+        printf("%d\n",n);
+        for(int i=1;i<=k;i++)
+        {
+            temp=nums[n-1];
+            for(int j=0;j<n-1;j++){
+                nums[n-1-j]=nums[n-2-j];
+            }
+            nums[0]=temp;
+        }
+        for(int i=0;i<n;i++)
+        printf("%d ",nums[i]);
+        return 0;
 }
