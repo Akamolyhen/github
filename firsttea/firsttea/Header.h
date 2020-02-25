dd30e945ed25e137e93281020e69755280bedc37
Akamolyhen <907446875@qq.com>
2020-02-26 06:20:54 +0800

    1

---
+++ firsttea/firsttea/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  firsttea
+//
+//  Created by 恶龙咆哮 on 2020/2/26.
+//  Copyright © 2020 秦伟钦. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include<stdio.h>
+#endif /* Header_h */
--- firsttea/firsttea/main.c
+++ firsttea/firsttea/main.c
@@ -6,10 +6,24 @@
 //  Copyright © 2020 秦伟钦. All rights reserved.
 //
 
-#include <stdio.h>
+#include "Header.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    int x;
+    scanf("%d",&x);
+    int a[x];
+    while(x--)
+    {
+        a[x]=x;
+        printf("%d\t",a[x]);
+    }
+    printf("hehe\n");
+    char c='1';
+    printf("%c",c);
+
+    
+    
     return 0;
 }
