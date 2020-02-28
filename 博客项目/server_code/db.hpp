188b17b0f2e260cd5dc758396fa0ba83154f1ec1
Akamolyhen <907446875@qq.com>
2020-02-28 11:11:01 +0800

    55

---
+++ 博客项目/server_code/db.hpp
@@ -0,0 +1,15 @@
+//创建一些相关的类来封装数据库的操作
+
+
+#include<cstdio>
+#include<cstdlib>
+#include<mysql/mysql.h>
+
+namespace blog_system{
+	static MySQL* MySQLInit(){
+
+	}
+	static void MySQLRelease(){
+		
+	}
+}//end blog_system
