7702eeb8af3e7c44eb6c33d25943ce3690efbdf2
Akamolyhen <907446875@qq.com>
2020-02-29 20:31:41 +0800

    23

---
+++ Window/Window/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Window
+//
+//  Created by 恶龙咆哮 on 2020/2/29.
+//  Copyright © 2020 秦伟钦. All rights reserved.
+//
+
+#include <stdio.h>
+#include "SDL.h"
+
+int main()
+{
+    SDL_Window *window;
+    SDL_Init(SDL_INTI_VIDEO);
+    window;
+}
