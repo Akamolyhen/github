a8c07e4398086968ede89f268d49d2451c84d2a9
Akamolyhen <907446875@qq.com>
2020-03-01 08:47:10 +0800

    博客项目

--- 博客项目/server_code/db.hpp
+++ 博客项目/server_code/db.hpp
@@ -7,9 +7,47 @@
 
 namespace blog_system{
 	static MySQL* MySQLInit(){
+		//1、创建一个句柄
+		MYSQL* connect_fd = mysql_init(NULL);
+		//2、和数据库建立链接
+		if( mysql_real_connect(connect_fd,"127.0.0.1","root",
+                      "","blog_system", 3306,NULL,0 )==NULL)
+   {
+       printf("链接失败!%s\n",mysql_error(connect_fd));
+       return NULL;
+   }
+   //	设定字符编码
+   		mysql_set_character_set(connect_fd,"utf8");
+   		return connect_fd;
+	}
+
+
+	static void MySQLRelease(MYSQL* connect_fd){
+		//释放句柄并断开链接
+		mysql_close(connect_fd);
 
 	}
-	static void MySQLRelease(){
-		
+
+	//创建一个类，用于操作博客表的类
+	class BlogTable{
+	public:
+		BlogTable(MYSQL* connect_fd){
+			//通过这个构造函数获取到一个数据库的操作句柄
+		}
+		bool Insert(){
+
+		}
+		bool SelectAll(){
+
+		}
+		bool Selectone(){
+
+		}
+		bool Update(){
+
+		}
+		bool Delete(){
+			
+		}
 	}
 }//end blog_system
