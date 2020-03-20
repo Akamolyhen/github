1291ae940407254209a2ebb89f769de204dbefeb
Akamolyhen <907446875@qq.com>
2020-03-21 03:28:46 +0800

    3

--- Object C/Object/Object/main.m
+++ Object C/Object/Object/main.m
@@ -6,7 +6,7 @@
 //  Copyright © 2020 秦伟钦. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+/*#import <Foundation/Foundation.h>
 
 @interface Fraction : NSObject
 
@@ -61,3 +61,44 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+*/
+/*#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello,Objective-C!");
+        
+    }
+    return 0;
+}
+*/
+#import <Foundation/Foundation.h>
+
+BOOL areIntsDifferent(int thing1,int thing2){
+    if(thing1==thing2){
+        return NO;}
+    else{
+        return YES;}
+}
+
+
+NSString *boolString (BOOL yesNO)
+{
+    if(yesNO==NO){
+        return(@"NO");
+    }
+    else{
+        return (@"YES");}
+}
+
+int main(){
+    BOOL areTheyDifferent;
+    areTheyDifferent=areIntsDifferent(5, 5);
+    NSLog(@"are %d and %d different? %@",5,5,boolString(areTheyDifferent));
+    areTheyDifferent=areIntsDifferent(23, 45);
+    NSLog(@"are %d and %d different? %@",23,45,boolString(areTheyDifferent));
+    return 0;
+    
+}
+
