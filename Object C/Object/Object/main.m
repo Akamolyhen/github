03388acf0ec1882e3f1000876f42c2b5d3c5da1b
Akamolyhen <907446875@qq.com>
2020-03-23 09:37:10 +0800

    5

--- Object C/Object/Object/main.m
+++ Object C/Object/Object/main.m
@@ -106,7 +106,7 @@ int main(){
 
 // --------------------------------------------------
 // constants for the different kinds of shapes and their colors
-
+/*
 typedef enum {
     kRedColor,
     kGreenColor,
@@ -337,4 +337,106 @@ int main (int argc, const char * argv[])
     return (0);
     
 } // main
+
+*/
+/*
+
+
+// --------------------------------------------------
+
+@interface Tire : NSObject
+@end // Tire
+
+
+@implementation Tire
+
+- (NSString *) description
+{
+    return (@"I am a tire. I last a while");
+} // description
+
+@end // Tire
+
+
+
+// --------------------------------------------------
+
+@interface Engine : NSObject
+@end // Engine
+
+
+@implementation Engine
+
+- (NSString *) description
+{
+    return (@"I am an engine.  Vrooom!");
+} // description
+
+@end // Engine
+
+
+// --------------------------------------------------
+
+@interface Car : NSObject
+{
+    Engine *engine;
+    Tire *tires[4];
+}
+- (Engine *) engine;
+-(void) setEngine:(Engine*) newEngine;
+-(Tire*) tireAtIndex:(int) index;
+-(void) setTire:(Tire*) tire
+        atIndex:(int) index;
+
+- (void) print;
+
+@end // Car
+
+
+@implementation Car
+
+- (id) init
+{
+    if (self = [super init]) {
+        engine = [Engine new];
+        
+        tires[0] = [Tire new];
+        tires[1] = [Tire new];
+        tires[2] = [Tire new];
+        tires[3] = [Tire new];
+    }
+    
+    return (self);
+    
+} // init
+
+- (void) print
+{
+    NSLog (@"%@", engine);
+    
+    NSLog (@"%@", tires[0]);
+    NSLog (@"%@", tires[1]);
+    NSLog (@"%@", tires[2]);
+    NSLog (@"%@", tires[3]);
+    
+} // print
+
+@end // Car
+
+
+// --------------------------------------------------
+
+int main (int argc, const char * argv[])
+{
+    Car *car;
+    
+    car = [Car new];
+    [car print];
+    
+    return (0);
+    
+} // main
+
+*/
+
 
