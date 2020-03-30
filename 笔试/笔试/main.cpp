b046579c9abee4183e37121886ab441268fef825
Akamolyhen <907446875@qq.com>
2020-03-30 16:51:52 +0800

    1

---
+++ 笔试/笔试/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  笔试
+//
+//  Created by 恶龙咆哮 on 2020/3/30.
+//  Copyright © 2020 秦伟钦. All rights reserved.
+//
+#define High 100
+#include <iostream>
+#include<math.h>
+
+
+int main(int argc, const char * argv[]) {
+    static float ans1=High,ans2=High/2;
+    static float high=High;
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        if(i!=0)
+        {
+             high=high/2;
+             ans1=ans1+high*2;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(i!=0)
+        ans2=ans2/2;
+    }
+    
+    printf("%f\t%f",ans1,ans2);
+    return 0;
+}
