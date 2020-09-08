1db4fe4f376455339c07e438d97a0c52b4a27fea
Akamolyhen <907446875@qq.com>
2020-09-08 19:31:47 +0800

    饿

--- 笔试/笔试/笔试/笔试/main.cpp
+++ 笔试/笔试/笔试/笔试/main.cpp
@@ -106,4 +106,59 @@ int main()
 }*/
 
 
+//安排机器
+/*#include<iostream>
+using namespace std;
+#define LL long long
+const int maxn=1e5+10;
+struct node{
+    int x,y;
+}e[maxn],f[maxn];
+int cnt[105];//记录可使用的任务难度
+int cmp(node a,node b)
+{
+    if(a.x==b.x)
+        return a.y>b.y;
+    return a.x>b.x;
+}
+
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        cin>>e[i].x>>e[i].y;
+    for(int j=0;j<m;j++)
+        cin>>f[j].x>>f[j].y;
+    sort(e,e+n,cmp);
+    sort(f,f+m,cmp);
+    int num=0;
+    LL ans=0;
+    memset(cnt,0,sizeof(cnt));
+    int i,j,k;//i：任务数，j：机器数，k：难度等级
+    for(i=0,j=0;i<m;i++)
+    {
+        while(j<n&&e[j].x>=f[i].x)//判断可用机器和所用时间
+        {
+            cnt[e[j].y]++;//把该机器难度记录
+            j++;
+        }
+        for(k=f[i].y;k<=100;k++)//将此时任务的难度系数给k，看cnt这个数组所在的难度里有无机器满足
+        {
+            if(cnt[k])
+            {
+                num++;
+                cnt[k]--;
+                ans+=200*f[i].x+3*f[i].y;
+                break;
+            }
+        }
+    }
+    cout<<num<<' '<<ans<<endl;
+    return 0;
+}
+*/
+
+
 
