dd9a6b1626fdf67484cfa236f46a7edc73ef6c63
Akamolyhen <907446875@qq.com>
2020-09-11 09:14:10 +0800

    ;

--- 笔试/笔试/笔试/笔试/main.cpp
+++ 笔试/笔试/笔试/笔试/main.cpp
@@ -201,7 +201,7 @@ int main()
     return 0;
 }
 */
-
+/*
 //纸牌游戏
 #include<iostream>
 using namespace std;
@@ -232,3 +232,89 @@ int main()
 }
 
 
+*/
+
+
+//求幂
+/*
+ 对于每一个数，分两种情况：
+ 1.a==c，这种情况共有n*n (对应1)+n*(n-1) (对应2-n)种情况，就是a,c底数为1时，有n*n种情况；a==c且不等于1时有n-1种（2～n），bd相等的情况有n种（1～n）。
+2.a != c, 遍历左右值即可，对于一对a,c有n / (x和y中的较大值/ (x和y的公约数)) * 2种情况
+ a^b=c^d----->(i^x)^b = (i^y)^d
+ 遍历i，且i^x<=n、j^y<=n,求出x和y的范围，除去底数相同的情况，i*i必定小于n
+ 遍历x和y，取出x和y的最大公约数放到底数做i的指数，(i^m)^n1*b = (i^m)^n2*d
+ 转换成求n1*b==n2*d 有多少种情况，
+ x和y的最大值除以b/d使得等式成立，n1>n2，b、d<n
+ 所以构造d为n1的倍数即可，总的倍数有n/n1个。同时等式两边可以对调，因此*2
+*/
+/*#include<iostream>
+#include<set>
+using namespace std;
+int MOD = 1000000000 + 7;
+/// 返回最小公约数
+int gcd(int a,int b){
+    return (a % b == 0) ? b : gcd(b,a%b);
+}
+int main() {
+    int n;
+    cin>>n;
+    //int会越界，且还需要强制类型转换
+    long long ans =(long long) n*(n*2-1) % MOD;//a==c
+    set<int> s;
+    //遍历i,因为去除了底数相同的情况,所以底数不等的情况下：底数必定大于1，次方必定大于1——>底数小于n的开方
+    for (int i = 2; i*i <= n; i++){
+        if (s.find(i)!=s.end()) continue;//在s中找i这个数，s.find() 如果没找到返回s.end()
+        int tmp = i;
+        int cnt = 0;
+        //满足条件的底数，同时求x和y的范围
+        while(tmp <= n) {
+            s.insert(tmp);
+            tmp = tmp * i;
+            cnt++;
+        }
+        //相同的公约数，放到下面，反正不会大于n//n / (x和y中的较大值/ (x和y的公约数)) * 2
+        for (int k = 1; k <= cnt; k++) {
+            for (int j = k + 1; j <= cnt; j++) {
+                ans =(ans+(n / (j / gcd(k, j) ) * (long)2 )) % MOD;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+*/
+
+
+//整除
+#include<iostream>
+using namespace std;
+const int maxn=1e5+5;
+int tmp[maxn];
+int n;
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int k=i;
+        for(int j=2;j*j<=n;j++){
+            int s=0;
+            while(k%j==0){
+                s++;
+                k/=j;
+            }
+            tmp[j]=max(tmp[j],s);
+        }
+        if(k>1)
+            tmp[k]=max(tmp[k],1);
+    }
+    long long res=1;
+    for(int i=1;i<=1e5;i++)
+    {
+        for(int j=0;j<tmp[i];j++){
+            res=res*i%987654321;
+        }
+    }
+    cout<<res<<endl;
+    return 0;
+}
