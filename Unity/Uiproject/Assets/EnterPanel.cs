08a0bf0cdf80b7a27cbde4469b7aa7379dcaed8d
Akamolyhen <907446875@qq.com>
2020-10-13 16:38:00 +0800

    2

---
+++ Unity/Uiproject/Assets/EnterPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterPanel : MonoBehaviour
+{
+    public Button btn_Enter;
+    // Start is called before the first frame update
+    //start 在物体显示出来自动调用一次
+    void Start()
+    {
+        btn_Enter.onClick.AddListener(ShowLoginPanel);
+    }
+    private void ShowLoginPanel()
+    {
+        Debug.Log("登陆按钮被单击");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
