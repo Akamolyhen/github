16bb79609ff60f8dcba315c385a9d0847772ce35
Akamolyhen <907446875@qq.com>
2020-11-05 11:08:39 +0800

    汉诺塔递归实现

--- 笔试/笔试/笔试/笔试/main.cpp
+++ 笔试/笔试/笔试/笔试/main.cpp
@@ -746,11 +746,38 @@ int main()
 */
 #include<bits/stdc++.h>
 using namespace std;
+//int main()
+//{
+//    char str[]="hello world";
+//    strcpy(str,"hi");
+//    cout<<str<<endl;
+//    cout<<str[3]<<endl;
+//    return 0;
+//}
+using namespace std;
+ 
+void move(int n,char A,char B,char C);
+int step;
 int main()
 {
-    char str[]="hello world";
-    strcpy(str,"hi");
-    cout<<str<<endl;
-    cout<<str[3]<<endl;
+    int n;
+    cout<<"请输入block数n:"<<endl;
+    cin>>n;
+    move(n,'A','B','C');
     return 0;
+}
+void move(int n,char A,char B,char C)
+{
+    if(n==1)
+    {
+        step++;
+        cout<<"["<<step<<"]move 1# from "<<A<<" to "<<C<<endl;
+    }
+    else
+    {
+        move(n-1,A,C,B);
+        cout<<"["<<step<<"]move "<<n<<"# from "<<A<<" to "<<B<<endl;
+        step++;
+        move(n-1,B,A,C);
+    }
 }
